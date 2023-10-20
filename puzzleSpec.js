c49fceef7ae6f2d9a9e84acc44db64a1f803b535
Dispensers <121032668+Dispensers@users.noreply.github.com>
2023-10-20 16:25:35 +0100

    Add files via upload

---
+++ puzzleSpec.js
@@ -0,0 +1,90 @@
+/* Go with this one ... */
+let punterPuzzleSpec = {
+	number: 22,
+	solveBy: "30 Nov",
+	dispenserSpec: [undefined, "+7", "6-3", "09"],
+	targetSpec: "88",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "37-9+60",
+	solutionDispenseSequence: [2, 1, 2, 3, 1, 2, 3]
+};
+/*
+let punterPuzzleSpec = {
+	number: 22,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "-7", "6*k", "08"],
+	targetSpec: "88",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "37-9+60",
+	solutionDispenseSequence: [2, 1, 2, 3, 1, 2, 3]
+};
+/*
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "+97k", "6", "1-", "0", "2"],
+	targetSpec: "88",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "37-9+6021",
+	solutionDispenseSequence: [2, 1, 2, 3, 1, 2, 3]
+}; */
+/*
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "/9", "3-","6810"],
+	targetSpec: "28",
+	hintSpec: {numDots: 2, symbol:"3", isHere: true},
+	solutionExpression: "90-186/3",
+	solutionDispenseSequence: [1, 4, 2, 4, 3, 2, 1, 3]
+}; */
+/*
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "/9", "36-", "8", "1", "0"],
+	targetSpec: "28",
+	hintSpec: {numDots: 2, symbol:"3", isHere: true},
+	solutionExpression: "90-186/3",
+	solutionDispenseSequence: [1, 4, 2, 4, 3, 2, 1, 3]
+}; */
+/*
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "/9", "36-", "8", "10"],
+	targetSpec: "28",
+	hintSpec: {numDots: 2, symbol:"3", isHere: true},
+	solutionExpression: "90-186/3",
+	solutionDispenseSequence: [1, 4, 2, 4, 3, 2, 1, 3]
+}; */
+/*
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "/9", "6-", "38", "10"],
+	targetSpec: "28",
+	hintSpec: {numDots: 2, symbol:"3", isHere: true},
+	solutionExpression: "90-186/3",
+	solutionDispenseSequence: [1, 4, 2, 4, 3, 2, 1, 3]
+}; */
+/*
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "/9", "6-", "38", "0", "1"],
+	targetSpec: "147",
+	hintSpec: {numDots: 2, symbol:"3", isHere: false},
+	solutionExpression: "90-186/3",
+	solutionDispenseSequence: [1, 4, 2, 5, 3, 2, 1, 3]
+}; */
+/*
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "-1", "4*k", "71"],
+	targetSpec: "147",
+	hintSpec: {numDots: 2, symbol:"3", isHere: false},
+	solutionExpression: "11*14-7",
+	solutionDispenseSequence: [1, 2, 2, 3, 2, 1, 3]
+}; */
