da81f2868c019dd9e722ea220b02ebd20aba36e4
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-01-31 17:43:08 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,3 +1,48 @@
+
+let punterPuzzleSpec = {
+	number: 12,
+	solveBy: "6 Feb",
+	dispenserSpec: [undefined, "26", "4*", "/05"],
+	targetSpec: "200",
+	hintSpec: {numDots: 3, symbol:"0", isHere: false},
+	solutionExpression: "50/6*24",
+	solutionDispenseSequence: [3, 3, 3, 1, 2, 1, 2]
+};
+
+/*
+let punterPuzzleSpec = {
+	number: 15,
+	solveBy: "28 Jan",
+	dispenserSpec: [undefined, "-1", "4*1", "71"],
+	targetSpec: "147",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "81-16/7",
+	solutionDispenseSequence: [2, 1, 2, 3, 1, 2, 3]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 14,
+	solveBy: "28 Jan",
+	dispenserSpec: [undefined, "/61", "7", "91-8"],
+	targetSpec: "53",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "81-196/7",
+	solutionDispenseSequence: [2, 1, 2, 3, 1, 2, 3]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 13,
+	solveBy: "28 Jan",
+	dispenserSpec: [undefined, "333", "+", "-", "21"],
+	targetSpec: "-5",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "37-9+60",
+	solutionDispenseSequence: [2, 1, 2, 3, 1, 2, 3]
+};
+*/
+/*
 let punterPuzzleSpec = {
 	number: 22,
 	solveBy: "28 Jan",
@@ -7,6 +52,7 @@ let punterPuzzleSpec = {
 	solutionExpression: "37-9+60",
 	solutionDispenseSequence: [2, 1, 2, 3, 1, 2, 3]
 };
+*/
 /*
 let punterPuzzleSpec = {
 	number: 99,
